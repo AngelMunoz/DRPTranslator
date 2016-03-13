12dd34c37dc70cdfbdd2c1caa551bd00c4019d6a
Angel Daniel Munoz Gonzalez <scythes.lol@gmail.com>
2016-03-13 02:09:02 -0700

    Changes to be committed: 	modified:   .gitignore 	new file:   .travis.yml 	modified:   package.json 	new file:   tests/index.test.ts 	modified:   tsconfig.json 	new file:   tsd.json

---
+++ tests/index.test.ts
@@ -0,0 +1,20 @@
+/// <reference path="../typings/tsd.d.ts"/>
+import chai = require("chai");
+var expect = chai.expect;
+describe('Name of the test1', () =>{ // describe the test case
+    it('what should it do', () =>{ // descrbibe what this step should do
+        expect(true).to.be.true; // code the logic for the test
+    });
+    it("what else it should do", () => { // add steps as you need
+        expect("It also does this").to.include("It also does this");
+    });
+});
+
+describe('Name of the test2', () =>{ // describe the test case
+    it('what should it do2', () =>{ // descrbibe what this step should do
+        expect(true).to.be.true; // code the logic for the test
+    });
+    it("what else it should do2", () => { // add steps as you need
+        expect("It also does this").to.include("It also does this");
+    });
+});
