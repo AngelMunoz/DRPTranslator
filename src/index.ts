266906d6556bb9916929276d9f656c0db636e355
Angel Daniel Munoz Gonzalez <scythes.lol@gmail.com>
2016-03-13 10:15:47 -0700

    chore(build, test): added local coverage and added a test

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
-module DrpTranslator {
+export module DrpTranslator {
     "use strict";
     console.log('Hello World');
+
+    export function soup(str:string):string {
+        return `The soup has: ${str}`
+    }
 }
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,7 +1,12 @@
 /// <reference path="../typings/main.d.ts"/>
+/// <reference path="../src/index.ts"/>
 
 import chai = require("chai");
+import drptranslator = require("../src/index");
+
 var expect = chai.expect;
+var assert = chai.assert;
+var drp = drptranslator.DrpTranslator;
 describe('Name of the test1', () =>{ // describe the test case
     it('what should it do', () =>{ // descrbibe what this step should do
         expect(true).to.be.true; // code the logic for the test
@@ -14,6 +19,15 @@ describe('Name of the test1', () =>{ // describe the test case
 describe('Name of the test2', () =>{ // describe the test case
     it('what should it do2', () =>{ // descrbibe what this step should do
         expect(true).to.be.true; // code the logic for the test
+    });
+    it("what else it should do2", () => { // add steps as you need
+        expect("It also does this").to.include("It also does this");
+    });
+});
+
+describe('Soup', () =>{ // describe the test case
+    it('The soup must have noodles', () =>{ // descrbibe what this step should do
+        assert.equal("The soup has: noodles", drp.soup("noodles"));
     });
     it("what else it should do2", () => { // add steps as you need
         expect("It also does this").to.include("It also does this");
