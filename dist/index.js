e8212646519487ca1ffd945f7168d229eaeb2480
Angel D. Munoz <scythes.lol@gmail.com>
2018-02-11 01:33:46 -0600

    fix(all my shit again): fixing my damned releases

--- dist/index.js
+++ dist/index.js
@@ -10,17 +10,13 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var rna_1 = require("./symbols/rna");
+    exports.RNA = rna_1.RNA;
     var dna_1 = require("./symbols/dna");
+    exports.DNA = dna_1.DNA;
     var codon_1 = require("./symbols/codon");
+    exports.Codon = codon_1.Codon;
     var dna_translator_1 = require("./translators/dna.translator");
+    exports.DNATranslator = dna_translator_1.DNATranslator;
     var rna_translator_1 = require("./translators/rna.translator");
-    exports.default = {
-        Translators: {
-            DNATranslator: dna_translator_1.DNATranslator,
-            RNATranslator: rna_translator_1.RNATranslator,
-        },
-        RNA: rna_1.RNA,
-        DNA: dna_1.DNA,
-        Codon: codon_1.Codon
-    };
+    exports.RNATranslator = rna_translator_1.RNATranslator;
 });
