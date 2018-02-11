c057707c81d7299ff474beb0938f7623bbf09a0b
Angel D. Munoz <scythes.lol@gmail.com>
2018-02-11 01:28:42 -0600

    fix(all my shit): fix the bad releases i've done today

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,15 +1,5 @@
-import { RNA } from './symbols/rna';
-import { DNA } from './symbols/dna';
-import { Codon } from './symbols/codon';
-import { DNATranslator } from './translators/dna.translator';
-import { RNATranslator } from './translators/rna.translator';
-
-export default {
-  Translators: {
-    DNATranslator,
-    RNATranslator,
-  },
-  RNA,
-  DNA,
-  Codon
-}
+export { RNA } from './symbols/rna';
+export { DNA } from './symbols/dna';
+export { Codon } from './symbols/codon';
+export { DNATranslator } from './translators/dna.translator';
+export { RNATranslator } from './translators/rna.translator';
